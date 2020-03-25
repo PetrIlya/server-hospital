a305241842a769da27cf6103dd94d93c4d295ff2
PetrIlya <airkills@yandex.by>
2020-03-25 17:23:13 +0300

    Repository update.

--- src/main/java/com/seriouscompanyname/serverhospital/repository/RecordPackRepository.java
+++ src/main/java/com/seriouscompanyname/serverhospital/repository/RecordPackRepository.java
@@ -1,13 +1,29 @@
 package com.seriouscompanyname.serverhospital.repository;
 
+import com.google.common.collect.Lists;
 import com.seriouscompanyname.serverhospital.model.Record;
 import com.seriouscompanyname.serverhospital.model.RecordPack;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RecordPackRepository extends CrudRepository<RecordPack, Long> {
     RecordPack getRecordPackByName(String name);
+
+    default List<Record> getRecordPackPage(String name,
+                                           int recordPerPageValue,
+                                           int pageNumber) {
+        List<Record> page = null;
+        try {
+            page = Lists.partition(getRecordPackByName(name).getRecords(),
+                    recordPerPageValue).get(pageNumber);
+            return page;
+        } catch (Exception e) {
+            return page;
+        }
+    }
+
 }
